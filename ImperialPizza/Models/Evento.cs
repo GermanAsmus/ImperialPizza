9d1d5ae10d5979e24f2618b68e7e89b568ff1231
Facundo Rodriguez <facu_rodriguez_@hotmail.com>
2016-07-25 23:55:34 -0300

    Commit 1 FR 25/07/16

---
+++ ImperialPizza/Models/Evento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImperialPizza.Models
+{
+    public class Evento
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Evento(string pNombre, string pDescripcion, DateTime pFecha)
+        {
+            Nombre = pNombre;
+            Descripcion = pDescripcion;
+            Fecha = pFecha;
+        }
+    }
+}
---
+++ ImperialPizza/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImperialPizza.Models
+{
+    public class Pizza
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+
+        public Pizza()
+        {
+
+        }
+    }
+}
