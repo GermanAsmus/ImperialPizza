ad8aadddc20587485ea814505f47b46ec1d93c77
German <electronik.32@gmail.com>
2016-08-07 20:22:50 -0300

    CommitCalendario1 GA 07/08/16

---
+++ PizzaMVC/Scripts/myCalendar.js
@@ -0,0 +1,9 @@
+﻿var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+function ObtenerMes()
+{
+    var mes = document.getElementById('mesHeader');
+    mes.innerHtml = meses[DateTime.Now.Month];
+    return false;
+}
+   
